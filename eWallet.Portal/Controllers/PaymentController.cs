d5c7a0cab679532da81bad4a0a2e7e2a0763e579
huypayflow <huynq@payflow.vn>
2015-08-18 16:59:45 +0700

    Xong phan Thanh toan hoa don - Thanh toan bang TK Vi

--- eWallet.Portal/Controllers/PaymentController.cs
+++ eWallet.Portal/Controllers/PaymentController.cs
@@ -50,5 +50,34 @@ namespace eWallet.Portal.Controllers
             }
             return View("Result");
         }
+
+        public ActionResult Confirm(string transaction_type, string trans_id, long amount)
+        {
+            Helper.SendRequestOTP(((dynamic)Session["user_profile"])._id);
+            ViewBag.transaction_type = transaction_type;
+            ViewBag.trans_id = trans_id;
+            ViewBag.amount = amount;
+            return View();
+        }
+
+        public ActionResult ConfirmWithOTP(string transaction_type, string trans_id, long amount, string otp)
+        {
+            string request = @"{system:'web_frontend', module:'transaction',type:'two_way',function:'confirm_otp',request:{user_id:" + ((dynamic)Session["user_profile"])._id
+                + ",transaction_type:'" + transaction_type + "', trans_id:'" + trans_id + "', amount: " + amount + ", otp:'" + otp + "'}}";
+            dynamic result = JObject.Parse(Helper.RequestToServer(request));
+            if (result.error_code != "00")
+            {
+                ViewBag.Result = "không thành công";
+                if (result.response.url_redirect != null) return Redirect(result.response.url_redirect.ToString());
+                return View("Result");
+            }
+            else
+            {
+                ViewBag.Result = "thành công";
+                if (result.response.url_redirect != null) return Redirect(result.response.url_redirect.ToString());
+                ViewBag.Trans = result.response;
+                return View("Result");
+            }
+        }
 	}
 }
