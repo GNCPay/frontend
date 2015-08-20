59e6f8b10d8d472b1a306b967b9022588d58a069
giang kool <giangkoolpark@gmail.com>
2015-08-20 14:10:25 +0700

    comit

--- eWallet.Portal/Controllers/UpdateController.cs
+++ eWallet.Portal/Controllers/UpdateController.cs
@@ -9,22 +9,21 @@ namespace eWallet.Portal.Controllers
 {
     public class UpdateController : Controller
     {
-        public static Data.MongoHelper DataHelper = new Data.MongoHelper("mongodb://127.0.0.1:27017/ewallet_business", "ewallet_business");
-        public JsonResult profile(string password, string address, string personal_id, string personal_id_issued_date, string personal_id_issued_by)
+        //public static Data.MongoHelper DataHelper = new Data.MongoHelper("mongodb://127.0.0.1:27017/ewallet_business", "ewallet_business");
+       
+        public JsonResult profile(string Password, string Address, string personalid, string personalid_issueddate, string personalid_issuedby)
         {
             dynamic response = (dynamic)Session["user_profile"];
-            if (response.password != password)
+            if (response.Password != Password)
             {
-                response.password = Common.Security.GenPasswordHash(response.user_name, password);
+                response.Password = Common.Security.GenPasswordHash(response.user_name, Password);
             }
-                response.address = address;
-                response.personal_id = personal_id;
-                response.personal_id_issued_date = personal_id_issued_date;
-                response.personal_id_issued_by = personal_id_issued_by;
-                DataHelper.Save("profile", response);
-                Response.Redirect("~/Home/Index");
-           return Json(new { error_code = response.error_code.ToString(), error_message = response.error_message.ToString() }, JsonRequestBehavior.AllowGet);
-
-        }
+                response.Address = Address;
+                response.personalid = personalid;
+                response.personalid_issueddate = personalid_issueddate;
+                response.personalid_issuedby = personalid_issuedby;
+                Helper.DataHelper.Save("profile", response);
+                //return Json(new { error_code = response.error_code.ToString(), error_message = response.error_message.ToString() }, JsonRequestBehavior.AllowGet);
+           }
     }
 }
