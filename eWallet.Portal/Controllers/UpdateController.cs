c41fddf5015675efcd52173110d5d6576d174f9d
giang kool <giangkoolpark@gmail.com>
2015-08-20 15:15:42 +0700

    update profile

--- eWallet.Portal/Controllers/UpdateController.cs
+++ eWallet.Portal/Controllers/UpdateController.cs
@@ -10,20 +10,20 @@ namespace eWallet.Portal.Controllers
     public class UpdateController : Controller
     {
         //public static Data.MongoHelper DataHelper = new Data.MongoHelper("mongodb://127.0.0.1:27017/ewallet_business", "ewallet_business");
-       
-        public JsonResult profile(string Password, string Address, string personalid, string personalid_issueddate, string personalid_issuedby)
+
+        public JsonResult profile(string password, string Address, string personalid, string personalid_issueddate, string personalid_issuedby)
         {
             dynamic response = (dynamic)Session["user_profile"];
-            if (response.Password != Password)
+            if (response.password != password)
             {
-                response.Password = Common.Security.GenPasswordHash(response.user_name, Password);
+                response.password = Common.Security.GenPasswordHash(response.user_name, password);
             }
-                response.Address = Address;
-                response.personalid = personalid;
-                response.personalid_issueddate = personalid_issueddate;
-                response.personalid_issuedby = personalid_issuedby;
-                Helper.DataHelper.Save("profile", response);
-                //return Json(new { error_code = response.error_code.ToString(), error_message = response.error_message.ToString() }, JsonRequestBehavior.AllowGet);
-           }
+            response.Address = Address;
+            response.personalid = personalid;
+            response.personalid_issueddate = personalid_issueddate;
+            response.personalid_issuedby = personalid_issuedby;
+            Helper.DataHelper.Save("profile", response);
+            return Json(new { error_message = "cập nhật thành công !" }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
