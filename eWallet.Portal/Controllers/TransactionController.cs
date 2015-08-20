137cb84d4c1bbb9efc89e68d6f75f066fb69db53
giang kool <giangkoolpark@gmail.com>
2015-08-20 14:04:03 +0700

    update transaction

--- eWallet.Portal/Controllers/TransactionController.cs
+++ eWallet.Portal/Controllers/TransactionController.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver.Builders;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +11,26 @@ namespace eWallet.Portal.Controllers
 {
     public class TransactionController : Controller
     {
-        //
+
         // GET: /Transaction/
         public ActionResult List(string id)
         {
-            if (String.IsNullOrEmpty(id))
+          if (Session["user_profile"] != null)
             {
-                id = "Personal";
+                if (String.IsNullOrEmpty(id))
+                {
+                    id = "Personal";
+                }
+                //dynamic[] list_profile = App_Start.eWalletConfig.BusinessDataHelper.List("transactions", null, SortBy.Descending("system_created_time"));
+                //ViewBag.Type = id;
+                //ViewBag.List = list_profile;
+                //return View();
+                dynamic trans= (dynamic)Session["user_profile"];
+                dynamic[] list_profile = Helper.DataHelper.List("transactions", Query.EQ("created_by", trans._id), SortBy.Descending("system_created_time"));
+                ViewBag.Type = id;
+                ViewBag.List = list_profile;
             }
-            dynamic[] list_profile = App_Start.eWalletConfig.BusinessDataHelper.List("transactions",null,SortBy.Descending("system_created_time"));
-            ViewBag.Type = id;
-            ViewBag.List = list_profile;
             return View();
         }
-	}
+    }
 }
