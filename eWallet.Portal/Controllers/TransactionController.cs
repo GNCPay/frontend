f684a89733b154789483515eb27e4b47891216df
huypayflow <huynq@payflow.vn>
2015-08-27 11:05:01 +0700

    Fix transaction

--- eWallet.Portal/Controllers/TransactionController.cs
+++ eWallet.Portal/Controllers/TransactionController.cs
@@ -16,15 +16,12 @@ namespace eWallet.Portal.Controllers
         public ActionResult List(string id)
         {
             //id: transaction_type
-            if (Session["user_profile"] != null)
-            {
+           
                 //Nếu id = empty -> lay all giao dich cua user
                 //Neu id <> empty -> lay giao dich cua user ma co transaction_type = id
-                dynamic trans = (dynamic)Session["user_profile"];
-                dynamic[] list_profile = Helper.DataHelper.List("transactions", Query.EQ("created_by", trans._id), SortBy.Descending("system_created_time"));
+                dynamic[] list_profile = Helper.DataHelper.List("transactions", Query.EQ("created_by", User.Identity.Name), SortBy.Descending("system_created_time"));
                 ViewBag.Type = id;
                 ViewBag.List = list_profile;
-            }
             return View();
         }
     }
